078fd2c7b8f7a655f23c512766528f97097f04fb
tmavro <thomasmavr@gmail.com>
2022-05-16 10:38:14 +0200

    Create harryplotter.py

---
+++ harryplotter.py
@@ -0,0 +1,55 @@
+import csv 
+import re
+import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt
+
+# Create CSV file and writer
+f = open('data.csv', 'w', newline='')
+writer = csv.writer(f)
+
+# Regex matches for temp and time
+regex = re.compile(": (\d*\.\d*),.*: (\d*)")
+
+with open("data.txt") as data: 
+    # Go through each line of data.txt
+    for line in data: 
+        # See if it matches with regex
+        for match in regex.finditer(line): 
+            # Write a new row with regex matches
+            writer.writerow([match.group(1),match.group(2)]) 
+
+#Close writer and CSV file, data.csv
+f.close()
+
+# Read data.csv, defining column names and setting index column set to 'ms'.
+data = pd.read_csv('data.csv', names=['temp', 'ms', 'count'], index_col='ms')
+
+# Rewrite time column to panda timedelta milliseconds
+data.index = pd.to_timedelta(data.index, unit='ms')
+
+# Resample to calculate mean temperature and number of events every 15 s
+resamp = data.resample('15s').agg({'temp': ['mean', 'count']})
+
+# Flattens multiindex columns and fixes names 
+resamp.columns = resamp.columns.map('_'.join)
+
+print(resamp)
+
+resamp.to_csv('result.csv')
+
+sb.set_style("darkgrid")
+
+fig, ax =plt.subplots(1,2)
+
+res = sb.lineplot(x='ms', y='temp_mean', data=resamp, ax=ax[0])
+
+res.set(xlabel='Time [ms]', ylabel='Mean temperature [°C]')
+
+res2 = sb.scatterplot(x='temp_count', y='temp_mean', data=resamp, ax=ax[1])
+
+res2.set(xlabel='# events pr 30 minutes', ylabel='Mean temperature [°C]')
+
+plt.show()
+
+fig.savefig("result.pdf", bbox_inches='tight')
